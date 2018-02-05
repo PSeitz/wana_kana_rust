b88e7307cc1b7f7673f1b4982effb75ce958d5fc
Pascal Seitz <pascal.seitz@gmail.com>
2018-02-05 21:16:11 +0100

    fix pipes

--- src/bin/to_kana.rs
+++ src/bin/to_kana.rs
@@ -4,16 +4,19 @@ use std::env;
 use std::slice::SliceConcatExt;
 
 use std::io::{self, Read};
+use std::io::prelude::*;
 
 fn main() {
     let args: String = env::args().skip(1).collect::<Vec<String>>().join(" ");
     if args.len() > 0 {
         println!("{}", wana_kana::to_kana::to_kana(&args));
+    }else{
+        let mut buffer = String::new();
+        io::stdin().read_to_string(&mut buffer).unwrap();
+        if buffer.len() > 0 {
+            // println!("{}", wana_kana::to_kana::to_kana(&buffer));
+            io::stdout().write(wana_kana::to_kana::to_kana(&buffer).as_bytes()).unwrap();
+        }
     }
 
-    let mut buffer = String::new();
-    io::stdin().read_to_string(&mut buffer).unwrap();
-    if buffer.len() > 0 {
-        println!("{}", wana_kana::to_kana::to_kana(&buffer));
-    }
 }
--- src/bin/to_romaji.rs
+++ src/bin/to_romaji.rs
@@ -4,16 +4,19 @@ use std::env;
 use std::slice::SliceConcatExt;
 
 use std::io::{self, Read};
+use std::io::prelude::*;
 
 fn main() {
     let args: String = env::args().skip(1).collect::<Vec<String>>().join(" ");
     if args.len() > 0 {
         println!("{}", wana_kana::to_romaji::to_romaji(&args));
+    }else{
+        let mut buffer = String::new();
+        io::stdin().read_to_string(&mut buffer).unwrap();
+        if buffer.len() > 0 {
+            // println!("{}", wana_kana::to_romaji::to_romaji(&buffer));
+            io::stdout().write(wana_kana::to_romaji::to_romaji(&buffer).as_bytes()).unwrap();
+        }
     }
 
-    let mut buffer = String::new();
-    io::stdin().read_to_string(&mut buffer).unwrap();
-    if buffer.len() > 0 {
-        println!("{}", wana_kana::to_romaji::to_romaji(&buffer));
-    }
 }
